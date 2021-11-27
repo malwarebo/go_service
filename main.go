746e28604f59b802f2eb39367c8e54441d4f2b03
Irfan <irfnhm@gmail.com>
2021-11-27 12:09:37 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Artist string `json:"artist"`
+	Price float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Go Ahead", Artist: "Alicia Keys", Price: 1.99},
+	{ID: "2", Title: "Ready 2 Go", Artist: "Martin Solveig", Price: 7.99},
+	{ID: "3", Title: "Go", Artist: "Michael Jackson", Price: 11.99},
+}
+
+func main(){
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8080")
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var  newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
