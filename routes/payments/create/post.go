0af2c31a076f188f0f73791e10752a021818b395
Irfan <irfnhm@gmail.com>
2022-05-03 22:50:09 +0530

    define payment routes

---
+++ routes/payments/create/post.go
@@ -0,0 +1,11 @@
+// route entry point
+func createPayment(c *gin.Context) {
+	var  newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
